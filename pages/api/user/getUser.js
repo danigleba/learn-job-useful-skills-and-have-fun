6b5132870206a9efdac11fbfa2ed146253270ccd
Dani Gleba <danigleba@MacBook-Air-de-Dani.local>
2023-08-22 14:48:35 +0200

    getUser fix 2

--- pages/api/user/getUser.js
+++ pages/api/user/getUser.js
@@ -14,14 +14,14 @@ export default async function (req, res) {
     const usersRef = collection(db, "users")
     const userSnap = query(usersRef, where("email", "==", email))
     const queryUserSnap = await getDocs(userSnap)
-    try {
+    //try {
         queryUserSnap.forEach((doc) => {
             res.status(200).json({data: doc.data()})
         })
-    } catch (error) {
-          console.log(error)
-          res.status(500).json({message: "Server error"})
-      }
+    //} catch (error) {
+   //       console.log(error)
+      //    res.status(500).json({message: "Server error"})
+     // }
   } //else {
     //res.status(200).json({message: "Token not found"})
   //}
