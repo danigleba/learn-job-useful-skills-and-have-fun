98c8c4f717a75ab237fc6a9e29250c058f47b946
Dani Gleba <danigleba@MacBook-Air-de-Dani.local>
2023-09-18 17:47:12 +0200

    playlist ui fix

--- components/Playlist.js
+++ components/Playlist.js
@@ -37,12 +37,16 @@ export default function Playlist(props) {
                                         <button onClick={() => changeStep(index)} className="w-full">
                                                 <div className={`${((index)<= props.limitStep) ? "hover:bg-[#333533] hover:border-[#333533] hover:text-white duration-200" : ""}  text-[#333533] rounded-md px-1 py-2 ${(index == props.activeStep) ? "bg-[#333533] text-white" : "bg-[#f4f4f4] border-[#f4f4f4]"} ${(index > props.limitStep) ? "cursor-default" : "cursor-pointer"}`}>
                                                     <div className="text-center flex justify-center px-2 items-center space-x-2">
-                                                        <h1 className="truncate font-medium  text-sm sm:text-md">
-                                                            {item.title}
-                                                        </h1>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="#333533" className={`w-5 h-5 ${((index -1)>= props.limitStep) ? "inline-block" : "hidden"}`}>
-                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                                                        </svg>
+                                                        <div className="w-4/5">
+                                                            <h1 className="truncate font-medium  text-sm sm:text-md">
+                                                                {item.title}
+                                                            </h1>
+                                                        </div>
+                                                        <div className="1/5">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="#333533" className={`w-5 h-5 ${((index -1)>= props.limitStep) ? "inline-block" : "hidden"}`}>
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                                            </svg>
+                                                        </div>
                                                     </div>
                                                 </div>     
                                         </button>
